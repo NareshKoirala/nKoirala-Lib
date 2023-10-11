577dd2b87816ae1741f6dcc4992ca76576b042ec
Naresh Koirala <koiralanaresh10@gmail.com>
2023-10-10 19:08:34 -0600

    commit
    commit

--- sw_led.c
+++ sw_led.c
@@ -1,1 +1,23 @@
 #include "sw_led.h"
+#include "derivative.h"
+
+
+void SWL_Init (void)
+{
+    DDR1AD1 = 0b11100000,
+    ATD1DIEN1 = 0b00011111;
+}
+
+void SWL_ON (SWL_LEDColour led)
+{
+    PT1AD1 ^= led;
+}
+void SWL_OFF (SWL_LEDColour led)
+{
+    PT1AD1 = led;
+}
+void SWL_TOG (SWL_LEDColour led)
+{
+
+}
+
