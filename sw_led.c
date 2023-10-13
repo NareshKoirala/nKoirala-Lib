2b506bd3fcf3dece6e109a6b84b755ccb39f35dc
Naresh Koirala <koiralanaresh10@gmail.com>
2023-10-12 18:13:56 -0600

    Update sw_led.c

--- sw_led.c
+++ sw_led.c
@@ -10,14 +10,14 @@ void SWL_Init (void)
 
 void SWL_ON (SWL_LEDColour led)
 {
-    PT1AD1 ^= led;
+    PT1AD1 |= led;
 }
 void SWL_OFF (SWL_LEDColour led)
 {
-    PT1AD1 = led;
+    PT1AD1 &= ~led;
 }
 void SWL_TOG (SWL_LEDColour led)
 {
-
+    PT1AD1 ^= led;
 }
 
